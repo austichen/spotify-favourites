2d67401ab6df359a072addc2cf3f975f01f73797
unknown <itsaustinchen@gmail.com>
2020-03-23 14:39:53 -0400

    Move redirect url to env variable

--- src/components/molecules/LoginButton/index.tsx
+++ src/components/molecules/LoginButton/index.tsx
@@ -6,7 +6,7 @@ import './LoginButton.css'
 
 const LOGIN_URL_BASE = 'https://accounts.spotify.com/authorize'
 const RESPONSE_TYPE = 'token'
-const REDIRECT_URI = process.env.REACT_APP_ENV === 'prod' ? 'https://spotify-favourites.netlify.com/' : 'http://localhost:3000/'
+const REDIRECT_URI = process.env.REACT_APP_ENV === 'prod' ? process.env.REACT_APP_PROD_URL : 'http://localhost:3000/'
 const SCOPE = 'user-top-read'
 
 const loginUrl = encodeURI(`${LOGIN_URL_BASE}?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}`)
