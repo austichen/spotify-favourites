89e3acd0a6126346dc98b6cb1063868f310cabd3
unknown <itsaustinchen@gmail.com>
2020-03-24 22:17:47 -0400

    Disable auto-login

--- src/App.tsx
+++ src/App.tsx
@@ -16,41 +16,15 @@ const checkLoggedIn = (): boolean => {
       return true;
     }
   }
-  const tokenInfo = window.localStorage.getItem(TOKEN_LOCALSTORE_KEY)
-  if (tokenInfo !== null) {
-    const {accessToken, expiryTimestamp} : IAccessTokenInfo = JSON.parse(tokenInfo)
-    if (accessToken && expiryTimestamp > Date.now()) {
-      return true;
-    }
-  }
   return false;
 }
 
-const updateLocalStoreToken = (accessTokenInfo : IAccessTokenInfo) => {
-  const currentTokenInfo = window.localStorage.getItem(TOKEN_LOCALSTORE_KEY)
-  if (currentTokenInfo === null) {
-    window.localStorage.setItem(TOKEN_LOCALSTORE_KEY, JSON.stringify(accessTokenInfo))
-    return;
-  }
-  if (currentTokenInfo === null || JSON.parse(currentTokenInfo).accessToken !== accessTokenInfo.accessToken) {
-    window.localStorage.setItem(TOKEN_LOCALSTORE_KEY, JSON.stringify(accessTokenInfo))
-  }
-}
-
 const getTokenInfo = (): IAccessTokenInfo => {
-  if (window.location.hash) {
     const {access_token, expires_in} = parseUrlHash(window.location.hash)
     return {
       accessToken: access_token,
       expiryTimestamp: Date.now() + expires_in * 1000
     }
-  } else {
-    const tokenInfo = window.localStorage.getItem(TOKEN_LOCALSTORE_KEY)
-    if (tokenInfo === null) {
-      throw new Error('Unable to retrieve token from local storage')
-    }
-    return JSON.parse(tokenInfo)
-  }
 }
 
 const cleanHashFromHistory = () => {
@@ -70,7 +44,7 @@ const App : React.FC = () => {
     if (loggedIn) {
       const accessTokenInfo = getTokenInfo()
       cleanHashFromHistory()
-      updateLocalStoreToken(accessTokenInfo)
+      // updateLocalStoreToken(accessTokenInfo) // disable auto-login
       setAccessToken(accessTokenInfo.accessToken)
     }
   }, [])
--- src/components/molecules/LoginButton/index.tsx
+++ src/components/molecules/LoginButton/index.tsx
@@ -6,10 +6,11 @@ import './LoginButton.css'
 
 const LOGIN_URL_BASE = 'https://accounts.spotify.com/authorize'
 const RESPONSE_TYPE = 'token'
-const REDIRECT_URI = process.env.REACT_APP_ENV === 'prod' ? process.env.REACT_APP_PROD_URL : 'http://localhost:3000/'
+const REDIRECT_URI = process.env.REACT_APP_ENV === 'prod' ? `https://${window.location.host}/` : 'http://localhost:3000/'
 const SCOPE = 'user-top-read'
+const SHOW_DIALOG = 'true'
 
-const loginUrl = encodeURI(`${LOGIN_URL_BASE}?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}`)
+const loginUrl = encodeURI(`${LOGIN_URL_BASE}?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&show_dialog=${SHOW_DIALOG}`)
 
 const LoginButton = () => {
   return <Link className='login-button' url={loginUrl}>Login</Link>
